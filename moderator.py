cb9622ab539b42a75450081120a5cc8c38d66b75
Aarav Gupta <aarav.gupta01104@gmail.com>
2026-06-07 19:30:27 +0530

    doing

--- moderator.py
+++ moderator.py
@@ -2,4 +2,16 @@ import os
 import json
 
 
-file =  "bad_words"
+file =  "bad_words.json"
+
+
+class MODERATOR_BOT():
+    if not os.path.exists(file):
+        pass
+
+
+
+
+    
+        
+        
