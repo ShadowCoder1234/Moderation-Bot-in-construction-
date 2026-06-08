409d8259ed70a34a0db2e89c0a0bb133f4ad6e4e
Aarav Gupta <aarav.gupta01104@gmail.com>
2026-06-08 12:33:09 +0530

    Fixed changes

--- moderator.py
+++ moderator.py
@@ -6,8 +6,24 @@ file =  "bad_words.json"
 
 
 class MODERATOR_BOT():
-    if not os.path.exists(file):
-        pass
+    def __init__(self, file, text):
+        self.file = file
+        self.text = text
+        self.load = self.Load()
+    def Load(self):
+        if not os.path.exists(self.file):
+            return {
+                "idiot": 20,
+                "hacker": 50,
+                "malware": 90
+            }
+        with open(self.file, "r+") as f:
+            return json.load(f)
+    def Save(self):
+        with open(self.file, "w") as f:
+            json.dump(f, indent=2, sort_keys= True)
+        
+        
 
 
 
